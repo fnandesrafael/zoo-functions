d1530ae48575055dd4dd48a5412ea2f98d840b86
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-10-25 15:09:50 -0300

    reinicializaçao do trybe evaluation

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,7 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  // Comentário para correção do Trybe Evaluation
 }
 
 module.exports = getAnimalsOlderThan;
