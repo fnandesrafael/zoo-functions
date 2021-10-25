291eb2750ae25ac7d9385fa3c652e3e2d60d5872
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-10-25 14:37:10 -0300

    requisito 1, nova tentativa

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,11 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(...ids) {
-  const animals = [];
-  data.species.forEach((animal) => {
-    if (animal.id === ids) animals.push(animal);
+// Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/rest_parameters
+// Foi usado de referencia para a utilização de indefinidos parametros a Sintaxe Rest Parameters
+function getSpeciesByIds(...chosenAnimal) {
+  const animalsArray = [];
+  data.species.forEach((animalElement) => {
+    if (animalElement.id === chosenAnimal) animalsArray.push(animalElement);
   });
-  return animals;
+  return animalsArray;
 }
 
 module.exports = getSpeciesByIds;
