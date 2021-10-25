53672d1900bf5e1e9dc3c68243f1c355724096a7
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-10-25 15:45:14 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,8 +1,25 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+/**
+ * Referência utilizada para o método find() e every()
+ * Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+ * Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+ */
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-  // Comentário para correção do Trybe Evaluation
+  /**
+   * Constante que guarda todos os animais que possuirem o mesmo nome de espécie que foi inserido no parametro 'animal'
+   */
+  const animalsByName = species.find((chosenAnimal) => chosenAnimal.name === animal);
+  /**
+   * Constante que recebe os animais previamente filtrados pela constante animalsByName e aplica um novo filtro baseado na idade dos animais residentes comparadas à idade escolhida no parametro
+   */
+  const agedAnimals = animalsByName.residents.every((residentAnimal) => residentAnimal.age >= age);
+  /**
+   * Retorna verdadeiro se os animais escolhidos possuirem uma idade minima compativel com a idade escolhida no parametro, e falso caso contrário
+   */
+  return agedAnimals;
 }
 
 module.exports = getAnimalsOlderThan;
