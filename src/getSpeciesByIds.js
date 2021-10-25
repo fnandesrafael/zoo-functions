6deba9db08b9ac0a1d58608d25ba717c1de8f342
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-10-25 14:28:27 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const animals = [];
+  data.species.forEach((animal) => {
+    if (animal.id === ids) animals.push(animal);
+  });
+  return animals;
 }
 
 module.exports = getSpeciesByIds;
