1e79eb1d2a99ff080776149679b477e817d7d324
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-10-26 16:43:57 -0300

    nova tentativa requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -15,10 +15,12 @@ function getRelatedEmployees(managerId) {
    * Caso o retorno da primeira função seja positivo, a função getRelatedEmployees deve retornar um array contendo exatamente todos os membros que são gerenciados pelo gerenteEscolhido.
    * Nesse caso o método filter() pega cada membro e encontra o valor do manager
    * Após é usado o método find() compara se esse valor é igual ao parametro passado(managerID)
-   * Por fim caso seja igual, o método map() seleciona esses funcionários que possuem o mesmo gerente e então são concatenados em um Templeta Literal formado pelo primeiro e ultimo nome do funcionario
-   * Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/map
    */
     return employees.filter((member) => member.managers.find((manager) => manager === managerId))
+    /**
+    * Por fim caso seja igual, o método map() seleciona esses funcionários que possuem o mesmo gerente e então são concatenados em um Templeta Literal formado pelo primeiro e ultimo nome do funcionario
+    * Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+    */
       .map((member) => `${member.firstName} ${member.lastName}`);
   } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
