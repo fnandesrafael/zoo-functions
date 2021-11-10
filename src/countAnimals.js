569e7270353d5a670a8a9cd74f27b38bb7d1f3d6
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-11-10 11:47:12 -0300

    resolve countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,25 +1,28 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(chosenAnimal) {
-  if (chosenAnimal === undefined) {
-    const animals = {};
-    /**
-     * Usando o método ForEach(), quando não houverem parâmetros, o objeto animals que está vazio acima, receberá como chave o valor do nome do animal.
-     * Utilizando o parametro animal é possivel acessar esse valor e torná-lo a chave.
-     * Como valor desta chave foi usado o total de animais dessa espécie, expresso por animal.residents.length
-     */
-    species.forEach((animal) => { animals[animal.name] = animal.residents.length; });
-    return animals;
-    /**
-     * Foi utilizado o repositório do Dávio Lopes para entender a estrutura da segunda e terceira condição do requisito 5
-     * Link: https://github.com/tryber/sd-015-a-project-zoo-functions/pull/72
-     */
-  } const animalsNumber = species.find((animal) => animal.name === chosenAnimal.species);
-  if (chosenAnimal.sex === undefined) return animalsNumber.residents.length;
-  return animalsNumber.residents.filter((gen) => gen.sex === chosenAnimal.sex);
-}
+/**
+ * Para a resolução desse requisito, foram consultádos os repositórios dos alunos:
+ * Gabriel Matos, Link: https://github.com/tryber/sd-016-b-project-zoo-functions/pull/123
+ * Dávio Lopes, Link: https://github.com/tryber/sd-015-a-project-zoo-functions/pull/72
+ */
 
-// console.log(countAnimals({ species: 'lions', sex: 'female' }));
+function countAnimals(chosenAnimal) {
+  const animals = {};
+  // Nessa primeira parte, se o parâmetro for indefinido, a função deverá retornar um objeto com o nome de todas as espécies do zoológico como chave e sua quantidade de residẽntes como valor.
+  if (chosenAnimal === undefined) {
+    data.species.forEach((animal) => {
+      animals[animal.name] = animal.residents.length;
+    });
+    return animals;
+  }
+  // Caso o parâmetro passado seja definido, a função deverá retornar a espécie que primeiro condiz com o parâmetro pelo método find() e a quantidad ede residentes dessa mesma espécie.
+  const findAnimal = data.species.find((animal) => animal.species === chosenAnimal.name);
+  if (chosenAnimal.sex === undefined) {
+    return findAnimal.residents.length;
+  }
+  // Se for especificado o gênero da espécie, então será retornado somente a quantidade de animais da respectiva espécie e que possua o mesmo gênero.
+  const sameSex = findAnimal.residents.filter((animal) => animal.sex === chosenAnimal.sex).length;
+  return sameSex;
+}
 
 module.exports = countAnimals;
