355c3e40084f7271ced2e6af5e0fdb832f16a2a4
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-11-10 18:52:41 -0300

    resolve getOldestFromFristSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  // Encontra a primeira espécie gerenciada pelo funcionário que tiver o mesmo id que foi passado no parâmetro.
+  const findSpecie = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  // Retorna um array com o nome, sexo e idade dos animal da respectiva espécie.
+  const findAnimal = data.species.find((animal) => animal.id === findSpecie).residents
+  // Com o método sort(), são ordenados todos os residêntes por ordem de idade.
+    .sort((a, b) => b.age - a.age);
+  // Por fim é retornado um array com o animal mais velho
+  return Object.values(findAnimal[0]);
 }
+
+// console.log(getOldestFromFirstSpecies('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
 
 module.exports = getOldestFromFirstSpecies;
